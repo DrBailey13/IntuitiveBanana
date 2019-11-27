f164fabc831ebd1b51323700c708234d63ae40ed
Adam Wenger <adamwenger44@gmail.com>
2019-11-26 18:18:31 -0800

    search form done, trending songs

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,12 @@ function artistPage(artistName) {
         var name = $("<div>").text(response.artists[0].strArtist)
         $("#nameArtist").prepend(name)
 
-        var site = $("<div>").text(response.artists[0].strWebsite)
+        var site = $("<a>").text(response.artists[0].strWebsite).attr("href", "https://" + response.artists[0].strWebsite)
+
+
         $("#artistLink").prepend(site)
+        console.log(response.artists[0].strWebsite)
+        console.log(site)
     })
 }
 
@@ -27,13 +31,14 @@ function searchArtists(artistName) {
         method: "GET",
     }).then(function (response) {
         console.log(response)
-
+       
 
         for (i = 0; i < response.track.length; i++) {
-            var songName = $("<div>").text(response.track[i].strTrack)
+            var songName = $("<a>").text(response.track[i].strTrack).attr("href",  response.track[i].strMusicVid).append("<br>")
+    
             $(".info").prepend(songName)
         }
-        
+
     })
 }
 
@@ -58,3 +63,38 @@ $("#cancelBtn").on("click", function (event) {
     $("#nameArtist").empty()
     $("#artistLink").empty()
 })
+
+$("#submitBtn").on("click", function (event) {
+    event.preventDefault();
+
+    $("#artistName").val("");
+
+    $(".info").empty()
+    $(".top10").empty()
+    $("#nameArtist").empty()
+    $("#artistLink").empty()
+
+})
+
+
+function trendingSongs(trending) {
+
+    var queryURL = "http://theaudiodb.com/api/v1/json/1/trending.php?country=us&type=itunes&format=singles"
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+
+        for (i = 0; i < response.trending.length; i++) {
+            var trendingSong = $("<div>").text(response.trending[i].strTrack + "---" + response.trending[i].strArtist)
+    
+            $(".trending").append(trendingSong)
+        }
+       
+    })
+
+}
+trendingSongs()
